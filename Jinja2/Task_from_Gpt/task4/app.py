ae0ee5db0368909e8ef48fdeef92855c82cde7f5
ivan1995ivan <111653497+ivan1995ivan@users.noreply.github.com>
2023-05-09 18:13:24 +0300

    added cycle for

--- Jinja2/Task_from_Gpt/task4/app.py
+++ Jinja2/Task_from_Gpt/task4/app.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    movie = {'title' : 'Titanic',
-             'year' : 1998,
-             'rating' : 9.9}
-    return render_template('movie.html', **movie)
+    movies = [
+        {'title': 'The Shawshank Redemption', 'year': 1994, 'rating': 9.3},
+        {'title': 'The Godfather', 'year': 1972, 'rating': 9.2},
+        {'title': 'Pulp Fiction', 'year': 1994, 'rating': 4.9}
+    ]
+    return render_template('movies.html', movies=movies)
 
 if __name__ == '__main__':
     app.run()
