6acd050badb16f513c7fa822ea6c2698e844467c
ivan1995ivan <111653497+ivan1995ivan@users.noreply.github.com>
2023-05-16 16:57:04 +0300

    app products+about+contact

--- Jinja2/Task_from_Gpt/task5/jinja_project/app.py
+++ Jinja2/Task_from_Gpt/task5/jinja_project/app.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/products')
+def products():
+    products = ['Product1', 'Product2', 'Product3']
+    return render_template('products.html', products=products)
+
 if __name__ == '__main__':
     app.run()
